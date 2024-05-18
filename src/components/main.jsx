6fff49e59db44a2a094d7174305659edf180deb6
rithinsai-dev <rithinsai.pamerla@gmail.com>
2024-05-18 16:46:42 +0530

    final

---
+++ src/components/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="https://img.freepik.com/free-vector/horizontal-sale-banner-template_23-2148897328.jpg?t=st=1716015482~exp=1716019082~hmac=d616084fa7b0103099080223ca594bc646c720f4daf4c1c11c1ec270d200994e&w=1060"
+            alt="Card"
+            height={500}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
